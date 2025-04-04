d61f42cf3684e9cf4c6ba5229ff9f9808ddd99b0
n4chh <iortego42madrid@gmail.com>
2025-04-04 23:17:21 +0200

    Cambios de permisos y solucionado error en install

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ cd src/alacritty
 for file in *;
 do
 	echo "Coping $file"
-	install -D $folder "~/.config/alacritty/$file"
+	install -D $file "$HOME/.config/alacritty/$file"
 done
 cd ../scripts
 for file in *;
 do
 	echo "Coping $file"
-	install -Dm 755 $folder "~/.config/scripts/$file"
+	install -Dm 755 $file "$HOME/.config/scripts/$file"
 done
 
