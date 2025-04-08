eabade8d948246bb7e74d9b600c56150a61c94ab
n4chh <iortego42madrid@gmail.com>
2025-04-08 13:17:57 +0200

    Rutas actualizadas

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 cd src/alacritty
 for file in *;
 do
-	echo "Coping $file"
+	echo "Coping alacritty/$file"
 	install -D $file "$HOME/.config/alacritty/$file"
 done
 cd ../scripts
 for file in *;
 do
-	echo "Coping $file"
+	echo "Coping scripts/$file"
 	install -Dm 755 $file "$HOME/.config/scripts/$file"
 done
 cd ../tmux
 for file in *;
 do
-	echo "Coping /tmux$file"
-	install -Dm 755 $file "$HOME/.config/scripts/$file"
+	echo "Coping tmux/$file"
+	install -Dm 755 $file "$HOME/.config/tmux/$file"
 done
 
