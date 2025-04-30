84890686d23e802614d166d3486c1b464bc6fede
n4chh <iortego42madrid@gmail.com>
2025-04-30 10:24:02 +0200

    Ficheros de instalacion agregados

--- install.sh
+++ install.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
+curl -SsLo /tmp/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.zip
 
-curl -SsLo /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip 
-
-sudo unzip -d /usr/local/share/fonts /tmp/Hack.zip
+sudo unzip -d /usr/local/share/fonts /tmp/FiraCode.zip
 
 cd src/alacritty
-for file in *;
-do
-	echo "Coping alacritty/$file"
-	install -D $file "$HOME/.config/alacritty/$file"
+for file in *; do
+    echo "Coping alacritty/$file"
+    install -D $file "$HOME/.config/alacritty/$file"
 done
 cd ../scripts
-for file in *;
-do
-	echo "Coping scripts/$file"
-	install -Dm 755 $file "$HOME/.config/scripts/$file"
+for file in *; do
+    echo "Coping scripts/$file"
+    install -Dm 755 $file "$HOME/.config/scripts/$file"
 done
 cd ../tmux
-for file in *;
-do
-	echo "Coping tmux/$file"
-	install -Dm 755 $file "$HOME/.config/tmux/$file"
+for file in *; do
+    echo "Coping tmux/$file"
+    install -D $file "$HOME/.config/tmux/$file"
 done
-
