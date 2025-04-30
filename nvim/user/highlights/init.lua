9734033cfa827e0531939792c1e08470313403bc
n4chh <iortego42madrid@gmail.com>
2025-04-30 10:22:50 +0200

    Merge con nueva base

--- nvim/user/highlights/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- this table overrides highlights in all themes
-  -- Normal = { bg = "None" },
-  -- NonText = { bg = "None" },
-  LineNr = { fg = "#70ff70" },
-  -- SignColumn = { bg = "None" },
-  -- StatusLine = { bg = "None" },
-  -- Comment = { fg = "#ffffff" },
-}
