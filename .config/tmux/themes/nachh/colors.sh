035c3492f72527e2445adfe93c09692f073135db
n4chh <iortego42madrid@gmail.com>
2025-09-02 08:14:40 +0200

    new prompt and tmux format update

--- .config/tmux/themes/nachh/colors.sh
+++ .config/tmux/themes/nachh/colors.sh
@@ -4,25 +4,21 @@ mode=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 BLUE=#3487ed
 WHITE=#ffffff
+BLACK=#000000
 
-PRIMARY=#c1fb33
-SECONDARY=#c1dbb3
+PRIMARY=#00ff99
+SOURCE=#33ccff
+SECONDARY=#537c8f
 TERCIARY=#faedca
 if [[ $mode == "'prefer-dark'" ]]; then
-	SOURCE=#c1fbb3
-	SOURCE=#c1fb33
     TAGBG=#494949
     TAGFGDIM=#909090
     TAGFG=terminal
 else
-	SOURCE=#218b53
     TAGFG=terminal
     TAGFGDIM=#909090
-    TAGBG=#d4d4d4
+    TAGBG=#eaeaea
 fi
 if [[ "$1" == "set" ]]; then
     echo "hey2" >/tmp/test
-    tmux set-option -g pane-border-style 'fg=terminal'
-    tmux set-option -g pane-active-border-style "fg=$SOURCE"
-    tmux set-option -g status-style "bg=terminal fg=$SOURCE"
 fi
--- .config/tmux/themes/nachh/theme.sh
+++ .config/tmux/themes/nachh/theme.sh
@@ -16,11 +16,19 @@ tmux set -g status-right "#($PWD/right_status.sh $PWD)"
 # ''
 # ''
 
+source $PWD/colors.sh
+
 tmux setw -g window-status-separator ' '
 tmux setw -g window-status-format "#($PWD/window_status.sh $PWD)"
 tmux setw -g window-status-current-format "#($PWD/window_status.sh $PWD active)"
 
 tmux set -g mouse on
+tmux set -g pane-border-status top
+tmux set -g pane-border-lines heavy
+tmux set -g pane-border-style 'fg=terminal'
+tmux set -g pane-active-border-style "fg=$PRIMARY"
+tmux set -g status-style "bg=terminal fg=$PRIMARY"
+tmux set -g pane-border-format ""
 
-# tmux set -g status-position top
+tmux set -g status-position top
 # tmux set -g status-position bottom
