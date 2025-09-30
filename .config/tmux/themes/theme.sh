7ac995df24baafbea7173a89bd05fb5ced37633a
n4chh <iortego42madrid@gmail.com>
2025-09-30 13:23:51 +0200

    cattpuccin update

---
+++ .config/tmux/themes/theme.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+tmux set -g default-terminal "screen-256color"
+# tmux set -g status-style "#(./colors.sh print)"
+tmux set -g status-justify left
+
+tmux set -g status on
+tmux set -g status-right-length 200
+tmux set -g status-left-length 40
+tmux set -g status-interval 1
+tmux set -g status-left "#($PWD/left_status.sh $PWD)"
+tmux set -g status-right "#($PWD/right_status.sh $PWD)"
+
+# icons
+# ''
+# ''
+# ''
+# ''
+
+source $PWD/colors.sh
+
+tmux set -g window-status-format "#($PWD/window_status.sh $PWD)"
+tmux set -g window-status-current-format "#($PWD/window_status.sh $PWD active)"
+
+tmux set -g mouse on
+
+tmux set -g pane-border-lines heavy
+tmux set -g pane-border-format ''
+tmux set -g pane-border-status top
+tmux set -g pane-border-style "fg=$OVERLAY0"
+tmux set -g pane-active-border-style "fg=$SAPPHIRE"
+tmux set -g status-style "bg=terminal fg=$SOURCE"
+
+tmux set -g message-style "fg=$TEXT bg=$SURFACE1"
+tmux set -g message-command-style "bg=$TEXT fg=$SURFACE1"
+
+tmux set -g status-position top
+# tmux set -g status-position bottom
