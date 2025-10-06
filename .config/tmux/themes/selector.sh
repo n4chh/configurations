7c8a3f0b51f15a719885e208b6d4190e47a6af24
n4chh <iortego42madrid@gmail.com>
2025-10-06 15:35:51 +0200

    theme selector

--- .config/tmux/themes/selector.sh
+++ .config/tmux/themes/selector.sh
@@ -1,11 +1,64 @@
 #!/usr/bin/env bash
-theme=nachh
-path="$HOME/.config/tmux/themes/$theme"
-[[ -d "$path" ]] || exit 1
-cd $path
-ls >/tmp/ls
-$path/colors.sh set
-ls / >/tmp/ls
-cd $path
-source $path/theme.sh
-# tmux source-file "$path/theme.tmux" >/tmp/test
+
+BASE_PATH="$HOME/.config/tmux/themes"
+DEFAULT_THEME="$BASE_PATH/aws"
+get_theme() {
+	local theme="$1"
+
+	if [[ -z "$theme" ]]; then
+		echo "$DEFAULT_THEME"
+		return
+	fi
+
+	if ! [[ -d "$BASE_PATH/$theme" ]]; then
+		echo "$DEFAULT_THEME"
+		return
+	fi
+
+	theme="$BASE_PATH/$theme"
+	if [[ -f "$theme/theme.sh" ]]; then
+		echo "$theme"
+	else
+		echo "$DEFAULT_THEME"
+	fi
+
+}
+
+change_theme() {
+	local script_path="$BASE_PATH/selector.sh"
+	local new_theme="$1"	
+	if [[ -z $new_theme ]]; then
+		return 1
+	fi
+	sed -i '' 's/\(DEFAULT_THEME="$BASE_PATH\/\).*"/\1'"$new_theme"'"/g' "$script_path"
+}
+
+source_theme() {
+	local theme="$(get_theme $1)"
+	cd "$theme"
+	"$theme/colors.sh" set
+	source "$theme/theme.sh"
+}
+
+
+
+fzf_theme_selector() {
+	local oifs=$IFS
+	IFS=$' '
+	local themes=($(/bin/ls -d ~/.config/tmux/themes/*/ | xargs basename))
+	IFS="$oifs"
+
+    echo "$themes" | fzf --prompt="Select session: " \
+            --header="Current: $DEFAULT_THEME" \
+			--preview="source $BASE_PATH/selector.sh; source_theme {}" \
+            --preview-window=right:60% \
+			--bind="esc:execute(source $BASE_PATH/selector.sh; source_theme)+abort"
+			--bind="enter:execute(source $BASE_PATH/selector.sh; source_theme {}; change_theme {})+accept"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+	command="display-popup -T '🎨Theme selector' -E 'source ~/.config/tmux/themes/selector.sh; fzf_theme_selector'"
+	tmux bind-key -Troot F3 "$command"
+	source_theme 
+fi
+
