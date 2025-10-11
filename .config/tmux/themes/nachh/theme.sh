ee90d663a7451c972f51850e2561dccad5e5c566
n4chh <iortego42madrid@gmail.com>
2025-10-11 10:49:34 +0200

    bindings added

--- .config/tmux/themes/nachh/theme.sh
+++ .config/tmux/themes/nachh/theme.sh
@@ -12,6 +12,35 @@ tmux set -g status-interval 1
 tmux set -g status-left "#($PWD/left_status.sh $PWD)"
 tmux set -g status-right "#($PWD/right_status.sh $PWD)"
 
+command="\
+if -F '#{==:#{mouse_status_range},window}' {\
+    select-window\
+} {\
+    if -F '#{m/r:^kill,#{mouse_status_range}}' {\
+        run-shell 'tmux kill-window -t #{s/^kill//:mouse_status_range}'\
+    } {\
+        if -F '#{==:#{mouse_status_range},new}' {\
+            new-window\
+        }\
+    }\
+}"
+
+tmux bind-key -Troot MouseDown1Status $command
+
+
+command='current_session="#{session_name}"\
+    selected=$(tmux list-sessions -F "#{session_name}" | \
+        fzf --prompt="Select session: " \
+            --header="Current: $current_session" \
+            --preview="tmux list-windows -t {}" \
+            --preview-window=right:60% \
+            --bind="ctrl-n:execute(tmux new-session -d -s)+reload(tmux list-sessions -F \"#{session_name}\")" \
+            --bind="ctrl-x:execute(tmux kill-session -t {})+reload(tmux list-sessions -F \"#{session_name}\")") 
+    [ -n "$selected" ] && tmux switch-client -t "$selected"'
+
+command="display-popup -T '🗄️Session selector' -E '$command'"
+tmux bind-key -T root MouseDown1StatusLeft "$command"
+tmux bind-key -Troot F1 "$command"
 
 tmux set -g window-status-format "#($PWD/window_status.sh $PWD)"
 tmux set -g window-status-current-format "#($PWD/window_status.sh $PWD active)"
