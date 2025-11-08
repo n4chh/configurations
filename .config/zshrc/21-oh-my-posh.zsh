19e05ff49edde6995ee05ec36b2e729b1bcd75e0
n4chh <iortego42madrid@gmail.com>
2025-11-08 13:55:56 +0100

    new mac theme

--- .config/zshrc/21-oh-my-posh.zsh
+++ .config/zshrc/21-oh-my-posh.zsh
@@ -43,7 +43,7 @@ _omp_detect_theme() {
   set_omp_theme
 
   if command -v oh-my-posh >/dev/null 2>&1; then
-	eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/zen.json)"
+	eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/mac.json)"
   fi
 fi
 
